b7295d43896f99fc38cb6271c383f3dc5b4c9124
NASEEFU RAHMAN KARUMANNIL <84283270+naseefu@users.noreply.github.com>
2024-12-26 13:10:48 +0530

    Update Contact.js

--- src/Component/Contact/Contact.js
+++ src/Component/Contact/Contact.js
@@ -66,7 +66,7 @@ const Contact = () => {
           <p ref={(el)=>sectionsRef.current[1]=el}  className='about1 text-gray-500 w-[90%] text-[18px] font-[500]'>My passion ignites in the world of technology. With ideas sparking like code coming to life, I craft solutions that bridge innovation and functionality.</p>
           <div className='mt-8'>
             <p ref={(el)=>sectionsRef.current[2]=el}  className='font-bold text-[20px] about2'>FILL THIS FORM OUT</p>
-            <input ref={(el)=>sectionsRef.current[3]=el}  type='text' placeholder='Name' className='about2 bg-[#0d0d0d] w-full md:w-[90%] mt-5 px-2 rounded-xl pl-5 py-5' style={{outline:"none"}} required/>
+            <input ref={(el)=>sectionsRef.current[3]=el}  type='text' placeholder='Name' className='about2 bg-[#0d0d0d] w-full md:w-[90%] mt-5 px-2 rounded-xl pl-5 py-5 border border-gray-800' style={{outline:"none"}} required/>
             <input ref={(el)=>sectionsRef.current[4]=el}  type='text' placeholder='E-Mail' className='about2 bg-[#0d0d0d] w-full md:w-[90%] mt-5 px-2 rounded-xl pl-5 py-5' style={{outline:"none"}} required/>
             <textarea ref={(el)=>sectionsRef.current[5]=el}  type='text' placeholder='Message' className='about2 bg-[#0d0d0d] w-full md:w-[90%] h-[200px] mt-5 px-2 rounded-xl pl-5 py-5' style={{outline:"none",resize:'none'}} required/>
             <button ref={(el)=>sectionsRef.current[6]=el}  className=' about2 text-center mt-5 bg-white text-black w-full md:w-[90%] py-5 border border-white hover:border-[rgba(255,255,255,0.15)] font-bold rounded-xl 
